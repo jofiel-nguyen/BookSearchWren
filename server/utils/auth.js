c923b1988c3722ec3195f310522dd50c0f7e2883
Wren <thitunguyen@my.lonestar.edu>
2023-07-25 20:59:17 -0500

    update auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,38 +6,32 @@ const expiration = '2h';
 
 module.exports = {
   // Function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // Check if the token is sent via the authorization header or query parameter
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      // Token sent via the authorization header (format: "Bearer <token>")
-      const [bearer, tokenValue] = token.split(' ');
-      if (bearer === 'Bearer' && tokenValue) {
-        token = tokenValue;
-      } else {
-        // Invalid authorization header format
-        return res.status(400).json({ message: 'Invalid authorization header format!' });
-      }
-    } else if (!token) {
-      // No token found in either authorization header or query parameter
-      return res.status(400).json({ message: 'You have no token!' });
+      token = token.split(' ').pop().trim();
     }
 
-    // Verify token and get user data out of it
+    if (!token) {
+      return req;
+    }
+
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'Invalid token!' });
     }
 
-    // Send to the next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
+
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
